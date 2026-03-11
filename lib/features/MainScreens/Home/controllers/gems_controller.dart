51ad1acf3929fa33efc821046f7e884135a55c4e
tufai <tufailhyder21@gmail.com>
2026-03-12 00:26:27 +0500

    initial commit

--- lib/features/MainScreens/Home/controllers/gems_controller.dart
+++ lib/features/MainScreens/Home/controllers/gems_controller.dart
@@ -2,11 +2,13 @@ import 'package:gemai/core/utils/popups/loaders.dart';
 import 'package:get/get.dart';
 import '../../../../data/repositories_authentication/gems/gems_repository.dart';
 import '../../../../models/popular_gemstone_model.dart';
+import '../../../../services/Firebase/storage_service.dart';
 
 class GemsController extends GetxController {
   static GemsController get instance => Get.find();
 
   final gemsRepository = GemsRepository();
+  final storage = StorageService();
   RxList<GemDetailModel> allGems = <GemDetailModel>[].obs;
   final isLoading = false.obs;
 
@@ -23,7 +25,6 @@ class GemsController extends GetxController {
 
       // Fetch popular gems from data source (Firestore, API, etc)
       final result = await gemsRepository.fetchGems();
-
 
       // Update the popular gems list
       allGems.assignAll(result);
